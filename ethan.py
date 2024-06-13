913f1bc75f3bd8fbfee2b2930fee739f28a81763
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-12 23:02:06 -0600

    added first print statement about tip calculator

--- ethan.py
+++ ethan.py
@@ -1,1 +1,60 @@
-print("Hi there")
+# calculations
+
+# print(2 + 5)
+
+# print(7-2)
+
+# print(3 * 4)
+
+# print(6/3) # 2.0 
+
+# print(6//3) # 2
+
+# print(7%7)
+
+# print(3**2)
+
+# first select all and for windows use contrl a then control  slash for multiple line comment
+
+# Assining variables 
+
+# a = 5  # here a is variable and 5 is value
+# a = "Ethan" 
+# b = "Vinodh"
+# c = "Python"
+
+# # print(a,b,c)
+# print(a+" "+b+" "+c)
+
+# a = int(input("Enter your number:")) # input function returns string and it will ask use to enter the value
+
+# #print("your number is "+str(a)) # type casting int to string (a)
+
+# print(f"your number is {a}")
+
+# b = "Vinodh"
+# print(b)
+
+# # interger data type 
+# my_integer = 5
+# print(my_integer)
+
+# # float data type
+# my_float = 5.0
+# print(my_float)
+
+# # string data type
+# my_string = "Vinodh"
+# print(my_string)
+
+# # boolean data type
+# my_boolean = True
+# print(my_boolean)
+
+
+# b = float(input("What is your lucky number:")) # What does input function return? and it will ask user to enter the value and it returns string
+# # print("your lucky number is "+str(b)) # type casting int to string (b)
+
+# print(f"your lucky number is {b}")
+
+print("Welcome to the tip calculator")
