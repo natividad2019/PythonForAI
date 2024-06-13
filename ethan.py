373ee90b0dc18d8b0e024fd916b7f38431580d6b
Kodandarao Chellapilli <briggs2019@go.byuh.edu>
2024-06-12 23:21:24 -0600

    finished the all lines of code for the tip calculator

--- ethan.py
+++ ethan.py
@@ -59,4 +59,17 @@
 
 print("Welcome to the tip calculator")
 bill = float(input("What was the total bill?"))
-input("How much tip would you like to give? 10, 12, or 15?")
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
+people = int(input("How many people to split the bill?"))
+
+# bill_with_tip = tip/100 * bill + bill 
+
+# bill_with_tip = bill * (1 + tip/100) / people
+
+tip_as_percent = tip / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people
+final_amout =round(bill_per_person, 2)
+print(f"Each person should pay: ${final_amout}")
+
